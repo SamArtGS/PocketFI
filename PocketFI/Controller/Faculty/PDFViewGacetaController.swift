526d4ef3548eb809d5f884d1e4678dd5147c8aee
Fernando Maceda <fernando.patricio.maceda@gmail.com>
2021-09-21 21:30:26 -0500

    Update PDFViewGacetaController.swift

--- PocketFI/Controller/Faculty/PDFViewGacetaController.swift
+++ PocketFI/Controller/Faculty/PDFViewGacetaController.swift
@@ -5,25 +5,41 @@
 //  Created by Samuel Arturo Garrido Sánchez on 9/17/21.
 //
 
+import WebKit
 import UIKit
 
 class PDFViewGacetaController: UIViewController {
+    
+    private let webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        if let resourceUrl = URL(string: "https://www.ingenieria.unam.mx/paginas/gaceta/pdf/2021/gaceta02_2021.pdf"){
+            let request = URLRequest(url: resourceUrl)
+            webView.load(request)
+        }
+        return webView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(webView)
+        setNavConfig(title: "Gaceta")
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.largeTitleDisplayMode = .never
     }
-    */
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationItem.largeTitleDisplayMode = .automatic
+    }
+    
 }
